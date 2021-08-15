bfeef91067a36c2bd654b4158539401ceaf03d24
ichimichi <ichimichilaribok@gmail.com>
2021-08-15 21:44:41 +0530

    renamed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mmorpg-online-clone'`, () => {
+  it(`should have as title 'toram-mmorpg-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mmorpg-online-clone');
+    expect(app.title).toEqual('toram-mmorpg-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mmorpg-online-clone app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'toram-mmorpg-web app is running!'
+    );
   });
 });
